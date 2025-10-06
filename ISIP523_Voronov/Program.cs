e1d04f2e2fd0c39c1cd4632621219df103b19104
JagerMeihter <kroec@bk.ru>
2025-10-06 16:21:22 +0300

    добавление базового меню и объявление функций работы...ЫАЫААЫАААЫЫАЫААЫ

--- ISIP523_Voronov/Program.cs
+++ ISIP523_Voronov/Program.cs
@@ -35,4 +35,66 @@ class Book
         Console.WriteLine($"ID: {BookID}, Название: {BookName}, Цена: {Price: денег}, Автор: {BookAuthor}, Жанр: {BookJenre}, Год: {BookYear}");
     }
 }
+class Program
+{
+    static List<Book> products = new List<Book>();
+    static int nextProductId = 1; // Счетчик для автоматической генерации ID
+    static void Main(string[] args)
+    {
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("\n=== Управление Книгами ===");
+            Console.WriteLine("1. Добавить книгу");
+            Console.WriteLine("2. Удалить книгу");
+            Console.WriteLine("3. Поиск книги");
+            Console.WriteLine("4. Авторы и книги");
+            Console.WriteLine("5. Цены и книги");
+            Console.WriteLine("6. Сортировка книг");
+            Console.WriteLine("7. Выход");
+            Console.Write("Выберите действие: ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    AddBook();
+                    break;
+                case "2":
+                    RemoveBook();
+                    break;
+                case "3":
+                    SearchBook();
+                    break;
+                case "4":
+                    AuthorBook();
+                    break;
+                case "5":
+                    PriceBook();
+                    break;
+                case "6":
+                    SortBook();
+                    break;
+                case "7":
+                    running = false;
+                    break;
+                default:
+                    Console.WriteLine("Неверный выбор!");
+                    break;
+            }
+        }
+    }
+
+    
+
+
+
+
+
+
+
+
+
+}
 
