b6e1a1daf5171db7a014b748ecbb76967b170903
JagerMeihter <kroec@bk.ru>
2025-10-06 15:47:19 +0300

    Объъявление и создание первых переменных

--- ISIP523_Voronov/Program.cs
+++ ISIP523_Voronov/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Book;
+class Book
+{
+    public int BookID { get; set; }
+    public string BookName { get; set; }
+    public string BookAuthor { get; set; }
+    public Jenre BookJenre { get; set; }
+    public int BookYear { get; set; }
+    public decimal Price { get; set; }
+    
+    public Book(int bookID , string name , string author , Jenre jenre , int year , decimal price)
+    {
+        BookID = bookID;
+        BookName = name;
+        BookAuthor = author;    
+        BookJenre = jenre;
+        BookYear = year;
+        Price = price;
+    }
+    public enum Jenre
+    {
+        Horror,
+        Detective,
+        Roman,
+        Fantastik,
+        Fantasy
+        
+    }
+}
