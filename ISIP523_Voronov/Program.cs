d96d51c4f671b6ddd955ef4eb3cf35429beee299
JagerMeihter <kroec@bk.ru>
2025-10-27 15:36:15 +0300

    пока списки и базовое меню

--- ISIP523_Voronov/Program.cs
+++ ISIP523_Voronov/Program.cs
@@ -1,6 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Tracing;
-using System.Linq;
-using System.Net.Quic;
-using System.Net.WebSockets;
+﻿
+using System.Reflection;
+
+List<string> StudentNamesRegistry = new List<string>();
+List<Student> StudentRoster = new List<Student>();
+List<Teacher> FacultyMembers = new List<Teacher>();
+List<Course> CourseCatalog = new List<Course>();
+
+bool programRunning = true;
+while (programRunning)
+{
+    Console.WriteLine("----------ГЛАВНОЕ МЕНЮ----------");
+    Console.WriteLine("1 - Зарегистрировать студента");
+    Console.WriteLine("2 - Показать список студентов");
+    Console.WriteLine("3 - Добавить преподавателя");
+    Console.WriteLine("4 - Показать список преподавателей");
+    Console.WriteLine("5 - Создать новый курс");
+    Console.WriteLine("6 - Показать все курсы");
+    Console.WriteLine("7 - Найти курсы по имени студента");
+    Console.WriteLine("8 - Показать полную базу данных");
+    Console.WriteLine("0 - Завершить программу");
+
+    
+}
