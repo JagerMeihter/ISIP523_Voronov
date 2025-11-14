2d33eb49d82f4a4609d0d8363d36c9ff1f9f3774
JagerMeihter <kroec@bk.ru>
2025-11-14 07:25:40 +0300

    Коммит: "Добавлена валидация в CarPart"

--- ConsoleApp1/CarPart.cs
+++ ConsoleApp1/CarPart.cs
@@ -12,10 +12,19 @@ namespace ConsoleApp1
         public decimal Price { get; set; }
         public int Quantity { get; set; }
 
-        
+
         public CarPart(string name, decimal price, int quantity)
         {
-            
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Название детали не может быть пустым");
+            if (price <= 0)
+                throw new ArgumentException("Цена должна быть положительной");
+            if (quantity < 0)
+                throw new ArgumentException("Количество не может быть отрицательным");
+
+            Name = name;
+            Price = price;
+            Quantity = quantity;
         }
     }
 }
