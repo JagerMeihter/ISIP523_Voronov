aa7e0b815176c420da0090acd3ef0e1af113c1ea
JagerMeihter <kroec@bk.ru>
2025-11-14 07:23:31 +0300

    Car создан

---
+++ ConsoleApp1/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Car
+    {
+        public string BrokenPart { get; set; }
+        public decimal RepairCost { get; set; }
+    }
+}
