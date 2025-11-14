4e5e923fba79cf8532f14c788dfd3cb6299b969c
JagerMeihter <kroec@bk.ru>
2025-11-14 07:22:07 +0300

    Warehouse создан

---
+++ ConsoleApp1/Warehouse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Warehouse
+    {
+        public List<CarPart> Parts { get; set; }
+        public decimal Balance { get; set; }
+
+        public void AddPart(CarPart part) { }
+        public bool HasPart(string partName) { return false; }
+        public void RemovePart(string partName) { }
+    }
+}
